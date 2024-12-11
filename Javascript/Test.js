1a88864e5841081ac8290e2efd385796990390e1
ivakuz29 <ivakuz29@gmail.com>
2024-12-11 15:39:42 +0100

    Hei

--- Javascript/Test.js
+++ Javascript/Test.js
@@ -3,5 +3,10 @@ function nightmode () {
 let container = document.querySelector("section")
 let p = container.querySelector("p")
 p.style.color = "white"
-p.innerHTML = "JavaScript - is fun"
+p.innerHTML = "Ivan"
+}
+
+function end () {
+
+    
 }
